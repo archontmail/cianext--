3e68510c1676b70c4b3571426195533caa6fdee9
archontmail <150447596+archontmail@users.noreply.github.com>
2025-03-04 21:58:12 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -66,9 +66,11 @@ async def get_mail(username, password, imap_server):
     with MailBox(imap_server).login(username, password) as mailbox:
         print('fetching...')
         for msg in mailbox.fetch(AND(seen=False)):
-            name = re.search('(.*) <' + msg.from_ + '>', msg.from_values.full)#.string.split(' ')
-            #lastName 
-            #data = { email: msg.from_, first_name: name }
+            attachments = []
+            name = re.search('(.*) <' + msg.from_ + '>', msg.from_values.full).group(1).split(' ')
+            lastName = name[-1]
+            firstName = ' '.join(name.pop[-1])
+            data = {email: msg.from_, first_name: firstName, last_name = lastName, subject = msg.subject, text = msg.text, html = msg.html, attachments = attachments}
             print(msg.date, msg.from_, msg.subject, msg.from_values,name.group(1), len(msg.text or msg.html))
 
 
