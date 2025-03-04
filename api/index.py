e7ae5675f7808511d3c1416cb5d1bb6b2d4b295f
archontmail <150447596+archontmail@users.noreply.github.com>
2025-03-04 23:00:12 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -69,7 +69,7 @@ async def get_mail(username, password, imap_server):
             attachments = []
             for a in msg.attachments:
                 print(a.filename)
-                print(a.payload)
+                #print(a.payload)
                 attachments.append(a)
             print(len(attachments))
             name = re.search('(.*) <' + msg.from_ + '>', msg.from_values.full).group(1).split(' ')
