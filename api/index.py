67b293e82d51240afcc3adee70a6c2e17d3289e6
archontmail <150447596+archontmail@users.noreply.github.com>
2025-03-05 17:54:48 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -29,7 +29,8 @@ imap_server = "imap.mail.ru"
 async def main(client):
     messages = await get_mail(username, password, imap_server)
     for msg in messages : 
-        print(retail_client.files_upload([]).keys())
+        result = await retail_client.files_upload([])
+        print(result.keys())
         result = await post_order(client, msg["first_name"], msg["last_name"], msg["email"], msg["subject"], msg["text"], msg["html"], msg["attachments"])
         return result    
 
